7810ee778ed73a01a7dd2845989e0fc046077652
Alex Flint <alex.flint@gmail.com>
2024-10-01 17:03:16 -0400

    finish and clean up dns experiment

--- dns-experiment/dnsproxy.go
+++ dns-experiment/dnsproxy.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net"
 	"os"
 
 	"github.com/alexflint/go-arg"
@@ -11,25 +13,36 @@ import (
 
 const upstreamDNS = "1.1.1.1:53" // TODO: get from resolv.conf and nsswitch.conf
 
-func handle(requestMsg *dns.Msg) (dns.RR, error) {
+// handle resolves IPv4 hosts according to net.DefaultResolver
+
+func handle(requestMsg *dns.Msg) ([]dns.RR, error) {
 	if len(requestMsg.Question) == 0 {
 		return nil, nil // this means no answer, no error, which is fine
 	}
 
 	question := requestMsg.Question[0]
-	log.Printf("got dns request for %v for %v", question.Qtype, question.Name)
+	log.Printf("got dns request for %v", question.Name)
+
+	ctx := context.Background()
 
 	// handle the request ourselves
-	if question.Qtype == dns.TypeA && question.Name == "example.com." {
-		log.Println("responding with example.com IP 44.33.22.11")
-		ip := "44.33.22.11"
-		rrline := fmt.Sprintf("%s A %s", question.Name, ip)
-		answer, err := dns.NewRR(rrline)
+	switch question.Qtype {
+	case dns.TypeA:
+		ips, err := net.DefaultResolver.LookupIP(ctx, "ip4", question.Name)
 		if err != nil {
-			log.Printf("error parsing %q: %v", rrline, err)
-			return nil, fmt.Errorf("error parsing rr: %w", err)
+			return nil, fmt.Errorf("the default resolver said: %w", err)
 		}
-		return answer, nil
+
+		var rrs []dns.RR
+		for _, ip := range ips {
+			rrline := fmt.Sprintf("%s A %s", question.Name, ip)
+			rr, err := dns.NewRR(rrline)
+			if err != nil {
+				return nil, fmt.Errorf("error constructing rr: %w", err)
+			}
+			rrs = append(rrs, rr)
+		}
+		return rrs, nil
 	}
 
 	log.Println("proxying the request...")
@@ -49,7 +62,7 @@ func handle(requestMsg *dns.Msg) (dns.RR, error) {
 	log.Printf("got answer from upstream dns server with %d answers", len(response.Answer))
 
 	if len(response.Answer) > 0 {
-		return response.Answer[0], nil
+		return response.Answer, nil
 	}
 	return nil, fmt.Errorf("not found")
 }
@@ -63,7 +76,7 @@ func Main() error {
 	dns.HandleFunc(".", func(w dns.ResponseWriter, req *dns.Msg) {
 		switch req.Opcode {
 		case dns.OpcodeQuery:
-			rr, err := handle(req)
+			rrs, err := handle(req)
 			if err != nil {
 				log.Printf("dns failed for %s with error: %v, continuing...", req, err.Error())
 				// do not abort here, continue on
@@ -71,15 +84,13 @@ func Main() error {
 
 			resp := new(dns.Msg)
 			resp.SetReply(req)
-			if rr != nil {
-				resp.Answer = append(resp.Answer, rr)
-			}
-
+			resp.Answer = rrs
 			w.WriteMsg(resp)
 		}
 	})
 
 	server := &dns.Server{Addr: args.Port, Net: "udp"}
+	server.ListenAndServe()
 	log.Printf("listening on %v...", server.Addr)
 	return server.ListenAndServe()
 }
